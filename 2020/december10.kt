30cb2574c0ec68ed98b11bdffb3bab333630c3fc
Gustavs Venters <gustavs.venters@mimacom.com>
2020-12-10 08:45:28 +0100

    Day 10

---
+++ 2020/december10.kt
@@ -0,0 +1,67 @@
+package solutions2020
+
+import java.io.File
+
+fun main() {
+    println(star1())
+    println(star2())
+}
+
+private fun input(): List<String> {
+    return File("src/solutions2020/december10.txt").readLines();
+}
+
+private fun star1(): Int {
+    val adapters = input().map(String::toLong).sorted()
+    var prev = 0L
+    var ones = 0
+    var threes = 0
+    for (curr in adapters) {
+        if (curr - prev == 3L) {
+            threes++
+        } else if (curr - prev == 1L) {
+            ones++
+        }
+        prev = curr
+    }
+    // your device's built-in adapter is always 3 higher than the highest adapter
+    threes++
+
+    println(ones)
+    println(threes)
+    return ones * threes
+}
+
+private fun star2(): Long {
+    val adapters = input().map(String::toLong).sorted()
+    var prev1 = 0L
+    var prev1paths = 1L
+    var prev2 = -99L
+    var prev2paths = 1L
+    var prev3 = -99L
+    var prev3paths = 1L
+
+    for (curr in adapters) {
+        var pathsToCurr = 0L
+        if (curr - prev1 <= 3L) {
+            pathsToCurr += prev1paths
+        }
+        if (curr - prev2 <= 3L) {
+            pathsToCurr += prev2paths
+        }
+        if (curr - prev3 <= 3L) {
+            pathsToCurr += prev3paths
+        }
+        prev3 = prev2
+        prev2 = prev1
+        prev1 = curr
+        prev3paths = prev2paths
+        prev2paths = prev1paths
+        prev1paths = pathsToCurr
+    }
+    return prev1paths
+}
+
+
+
+
